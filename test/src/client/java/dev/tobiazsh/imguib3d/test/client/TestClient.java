e175aa50284115b68564a1406987160bf6c8b3c5
Tobias <51285655+tobiazsh@users.noreply.github.com>
2026-08-10 15:52:17 +0200

    Add Test module for easy mod testing

---
+++ test/src/client/java/dev/tobiazsh/imguib3d/test/client/TestClient.java
@@ -0,0 +1,10 @@
+package dev.tobiazsh.imguib3d.test.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ test/src/main/java/dev/tobiazsh/imguib3d/test/Test.java
@@ -0,0 +1,10 @@
+package dev.tobiazsh.imguib3d.test;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Test implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
