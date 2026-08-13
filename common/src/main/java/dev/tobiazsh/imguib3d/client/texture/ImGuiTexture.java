f422e2836e752fee3ff835a53e95d5883de3913c
Tobias <51285655+tobiazsh@users.noreply.github.com>
2026-08-13 16:16:09 +0200

    Add TextureManager for ImGui

---
+++ common/src/main/java/dev/tobiazsh/imguib3d/client/texture/ImGuiTexture.java
@@ -0,0 +1,42 @@
+package dev.tobiazsh.imguib3d.client.texture;
+
+public interface ImGuiTexture {
+
+    /**
+     * Disposes the texture and releases any associated resources.
+     */
+    void dispose();
+
+    /**
+     * Uploads the texture data to the GPU. This method should be called before rendering the texture.
+     */
+    void upload();
+
+    /**
+     * Returns the texture id in the GPU. This id is used to bind the texture for rendering.
+     */
+    long getTextureId();
+
+    /**
+     * Checks if the texture has already been disposed. Disposed automatically should assume unusable.
+     */
+    boolean isDisposed();
+
+    /**
+     * Checks if the texture is currently bound to the GPU for rendering and can be used to do so.
+     * An unusable texture does not automatically mean that it is disposed, but a disposed texture is always unusable.
+     */
+    boolean isUsable();
+
+    /**
+     * Checks whether the texture has been uploaded to the GPU.
+     * Do not assume that a texture is usable just because it has been uploaded. There may be more factors that
+     * determine whether a texture is usable. It's best to use {@link #isUsable()} instead.
+     */
+    boolean isUploaded();
+
+    /**
+     * Returns the label associated with the texture. This label is used for identification and debugging purposes.
+     */
+    String getLabel();
+}
---
+++ common/src/main/java/dev/tobiazsh/imguib3d/client/texture/TextureManager.java
@@ -0,0 +1,61 @@
+package dev.tobiazsh.imguib3d.client.texture;
+
+import java.util.*;
+
+public final class TextureManager {
+
+    private static final TextureManager INSTANCE = new TextureManager();
+
+    public static TextureManager getInstance() {
+        return INSTANCE;
+    }
+
+    private final Map<Long, ImGuiTexture> textures = new HashMap<>(); // Uploaded textures, key is the texture ID
+    private final Set<ImGuiTexture> queuedTextures = new HashSet<>(); // Textures that are queued for upload, but not yet uploaded
+
+    public Map<Long, ImGuiTexture> getTextures() {
+        return Map.copyOf(textures);
+    }
+
+    public Set<ImGuiTexture> getQueuedTextures() {
+        return Set.copyOf(queuedTextures);
+    }
+
+    /**
+     * Registers a texture in the Map of USABLE textures. Do not register unuploaded or disposed textures.
+     * Any match in the queued textures will be removed from the queue and added to the usable textures.
+     *
+     * @apiNote Only register textures that are not disposed.
+     *          Disposed textures will be automatically removed from the manager.
+     *
+     * @param texture The texture to register.
+     */
+    public void registerTexture(ImGuiTexture texture) {
+        if (!texture.isUploaded() || texture.isDisposed())
+            throw new IllegalArgumentException("Cannot register an unuploaded or disposed texture");
+
+        queuedTextures.remove(texture);
+        textures.put(texture.getTextureId(), texture);
+    }
+
+    /**
+     * Queues a texture for upload. The texture will be uploaded in the next frame.
+     * Do not queue disposed textures.
+     * @param texture The texture to queue.
+     */
+    public void queueTexture(ImGuiTexture texture) {
+        if (texture.isDisposed())
+            throw new IllegalArgumentException("Cannot queue a disposed texture");
+
+        queuedTextures.add(texture);
+    }
+
+    public ImGuiTexture getTexture(long textureId) {
+        return textures.get(textureId);
+    }
+
+    public void clean() {
+        textures.values().removeIf(ImGuiTexture::isDisposed);
+        queuedTextures.removeIf(ImGuiTexture::isDisposed);
+    }
+}
