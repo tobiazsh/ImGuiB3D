ef9e631eb4ff271cd2f4f63c2535111c810ec3c7
Tobias <51285655+tobiazsh@users.noreply.github.com>
2026-08-15 20:45:08 +0200

    Remove version restriction since java version differentiate between java versions

--- buildSrc/src/main/kotlin/fabric-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/fabric-conventions.gradle.kts
@@ -10,18 +10,6 @@ repositories {
     }
 }
 
-java {
-    toolchain {
-        // Use Java 17 for compatibility with typical Fabric toolchains and to match local JDKs
-        languageVersion.set(JavaLanguageVersion.of(25))
-    }
-}
-
 dependencies {
     annotationProcessor("com.google.auto.service:auto-service:1.1.1")
-}
-
-tasks.withType<JavaCompile>().configureEach {
-    options.encoding = "UTF-8"
-    options.release.set(25)
 }
